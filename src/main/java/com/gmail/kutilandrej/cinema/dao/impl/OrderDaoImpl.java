7005f819436eb061e7d6e68580e9da96653a1623
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-05-28 11:32:22 +0300

    fixed LazyInitializedException

--- src/main/java/com/gmail/kutilandrej/cinema/dao/impl/OrderDaoImpl.java
+++ src/main/java/com/gmail/kutilandrej/cinema/dao/impl/OrderDaoImpl.java
@@ -4,7 +4,6 @@ import com.gmail.kutilandrej.cinema.dao.OrderDao;
 import com.gmail.kutilandrej.cinema.exception.DataProcessingException;
 import com.gmail.kutilandrej.cinema.lib.Dao;
 import com.gmail.kutilandrej.cinema.model.Order;
-import com.gmail.kutilandrej.cinema.model.Ticket;
 import com.gmail.kutilandrej.cinema.model.User;
 import com.gmail.kutilandrej.cinema.util.HibernateUtil;
 import java.util.List;
@@ -40,31 +39,14 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public List<Order> getOrderHistory(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            String hql = "FROM Order O LEFT JOIN FETCH O.tickets AS t JOIN FETCH t.movieSession "
-                    + "JOIN FETCH t.user WHERE O.user = :user";
+            String hql = "FROM Order O JOIN FETCH O.tickets AS T JOIN FETCH T.movieSession "
+                    + "AS MS JOIN FETCH MS.movie JOIN FETCH MS.cinemaHall "
+                    + "JOIN FETCH T.user WHERE O.user = :user";
             Query<Order> query = session.createQuery(hql, Order.class);
             query.setParameter("user", user);
-            List<Order> orders = query.getResultList();
-            fillTickets(orders);
-            return orders;
+            return query.getResultList();
         } catch (Exception e) {
             throw new DataProcessingException("Can't find Orders", e);
-        }
-    }
-
-    void fillTickets(List<Order> orders) {
-        for (Order order : orders) {
-            List<Ticket> tickets = order.getTickets();
-            for (int j = 0; j < order.getTickets().size(); j++) {
-                try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-                    String hql = "FROM Ticket T WHERE T.id = :id";
-                    Query<Ticket> query = session.createQuery(hql, Ticket.class);
-                    query.setParameter("id", tickets.get(j).getId());
-                    tickets.set(j, query.uniqueResult());
-                } catch (Exception e) {
-                    throw new DataProcessingException("Can't fill Tickets", e);
-                }
-            }
         }
     }
 }
