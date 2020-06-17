024cb77853d7554b70f1d611f88f8463780a406a
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-06-17 17:21:08 +0300

    deleted login method

--- src/main/java/com/gmail/kutilandrej/cinema/service/AuthenticationService.java
+++ src/main/java/com/gmail/kutilandrej/cinema/service/AuthenticationService.java
@@ -4,7 +4,5 @@ import com.gmail.kutilandrej.cinema.exception.AuthenticationException;
 import com.gmail.kutilandrej.cinema.model.User;
 
 public interface AuthenticationService {
-    User login(String email, String password) throws AuthenticationException;
-
     User register(String email, String login, String password) throws AuthenticationException;
 }
--- src/main/java/com/gmail/kutilandrej/cinema/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/gmail/kutilandrej/cinema/service/impl/AuthenticationServiceImpl.java
@@ -1,10 +1,13 @@
 package com.gmail.kutilandrej.cinema.service.impl;
 
 import com.gmail.kutilandrej.cinema.exception.AuthenticationException;
+import com.gmail.kutilandrej.cinema.model.Role;
 import com.gmail.kutilandrej.cinema.model.User;
 import com.gmail.kutilandrej.cinema.service.AuthenticationService;
+import com.gmail.kutilandrej.cinema.service.RoleService;
 import com.gmail.kutilandrej.cinema.service.ShoppingCartService;
 import com.gmail.kutilandrej.cinema.service.UserService;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,19 +17,16 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Autowired
     private UserService userService;
     @Autowired
+    private RoleService roleService;
+    @Autowired
     private ShoppingCartService shoppingCartService;
-
-    @Override
-    public User login(String email, String password) throws AuthenticationException {
-        return userService.findByEmail(email).filter(u -> u.getPassword()
-                .equals(password))
-                .orElseThrow(() -> new AuthenticationException("Incorrect email or password!"));
-    }
 
     @Override
     public User register(String email, String login, String password) {
         User user = new User(login, email);
         user.setPassword(password);
+        Role roleUser = roleService.getRoleByName("USER");
+        user.setRoles(Set.of(roleUser));
         userService.add(user);
         shoppingCartService.registerNewShoppingCart(user);
         return user;
